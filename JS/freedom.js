bd7f6b9315667ad5ebef077635a719d669a67f38
Lena Hoplamazian <lhoplama@lsoc.org>
2017-03-05 21:23:19 -0600

    freedom picscroll fixed

--- JS/freedom.js
+++ JS/freedom.js
@@ -1,4 +1,5 @@
 var freedompics=["https://apparatusmag.files.wordpress.com/2016/02/black-history-month.gif","http://artgallery.yale.edu/sites/default/files/event-images/ag-obj-210960_friedlander_woman_hand_on_hip_011917conversation_0.jpg","http://media3.artspace.com/media/bob_henriques/martin_luther_king_speaking_side/bob_henriques_martin_luther_king_speaking_side_320x320_c.jpg","http://americanhistory.si.edu/sites/default/files/A200%20Flyer%252C%201957%20Prayer%20Pilgrimage%20LC%20loan.jpg"]
+var current=0
 setInterval(function(){
 	if (current == freedompics.length-1){
 		current = 0;
